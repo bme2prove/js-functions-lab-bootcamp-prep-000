fa65aec2828f395fc01527fcbfbd224a8ba86438
Veaceslav Balan <bmichael0713@gmail.com>
2018-03-16 04:29:18 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
 // write your code below!
+function string (happyHolidays) {
+  return string "Happy holidays!"
+}
+function string  happyHolidaysTo(Misa) {
+  return string "Happy holidays,${Misa}!"
+}
